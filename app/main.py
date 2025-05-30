a578251cd80b3256995c5e709802195cefca8107
Franco Brizuela <brizuela.jfranco@gmail.com>
2025-05-30 14:04:11 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import sentry_sdk
 from fastapi import FastAPI
 from app.routes import task_routes
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
 sentry_sdk.init(
     dsn="https://97e3f6da0071d290b3ad05256eed40c2@o4509374230691840.ingest.us.sentry.io/4509374233837568",
